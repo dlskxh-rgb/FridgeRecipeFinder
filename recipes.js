0d9d02c4857760ae966f1ada55dd63e18f8dc936
dlskxh-rgb <dlskxh@gmail.com>
2026-09-09 13:26:33 +0900

    recipes.js 만들기

---
+++ recipes.js
@@ -0,0 +1,116 @@
+const recipes = [
+
+  {
+    name: "Vegetable Omelet",
+
+    ingredients: [
+      "egg",
+      "carrot",
+      "onion"
+    ],
+
+    description:
+      "A quick and healthy omelet with fresh vegetables."
+  },
+
+
+  {
+    name: "Egg Fried Rice",
+
+    ingredients: [
+      "egg",
+      "rice",
+      "onion"
+    ],
+
+    description:
+      "Simple fried rice made with eggs and vegetables."
+  },
+
+
+  {
+    name: "Chicken Vegetable Soup",
+
+    ingredients: [
+      "chicken",
+      "carrot",
+      "potato",
+      "onion"
+    ],
+
+    description:
+      "A warm and comforting chicken soup."
+  },
+
+
+  {
+    name: "Potato Soup",
+
+    ingredients: [
+      "potato",
+      "milk",
+      "onion"
+    ],
+
+    description:
+      "Creamy potato soup for an easy meal."
+  },
+
+
+  {
+    name: "Banana Oatmeal",
+
+    ingredients: [
+      "banana",
+      "oatmeal",
+      "milk"
+    ],
+
+    description:
+      "A simple and nutritious breakfast."
+  },
+
+
+  {
+    name: "Vegetable Fried Rice",
+
+    ingredients: [
+      "rice",
+      "carrot",
+      "onion",
+      "egg"
+    ],
+
+    description:
+      "A quick fried rice using simple ingredients."
+  },
+
+
+  {
+    name: "Tomato Egg Stir Fry",
+
+    ingredients: [
+      "tomato",
+      "egg",
+      "onion"
+    ],
+
+    description:
+      "A classic and easy tomato egg dish."
+  },
+
+
+  {
+    name: "Chicken Rice Bowl",
+
+    ingredients: [
+      "chicken",
+      "rice",
+      "egg"
+    ],
+
+    description:
+      "A simple protein-rich rice bowl."
+  }
+
+];
